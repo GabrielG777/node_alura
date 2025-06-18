ebdc83b33972e1a742c0ee8305c454335ce91b8d
= <=>
2025-06-18 17:03:47 -0300

    acrescentando metodos put, post e get

--- src/app.js
+++ src/app.js
@@ -2,32 +2,54 @@ import express from "express";
 
 const app = express();
 app.use(express.json());
+
 const livros = [
-  {
-    id: 1,
-    titulo: "Senhor dos anéis",
-  },
-  {
-    id: 2,
-    titulo: "O Hobbit",
-  },
+    {
+        id: 1,
+        titulo: "Senhor dos anéis",
+    },
+    {
+        id: 2,
+        titulo: "O Hobbit",
+    },
 ];
+
+function buscaLivros(id) {
+    return livros.findIndex(livros => {
+        return livros.id == Number(id);
+    })
+};
 
 //METODOS GETS
 app.get("/", (req, res) => {
-  res.status(200).send("Curso de node.js");
+    res.status(200).send("Curso de node.js");
 });
 app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
+    res.status(200).json(livros);
+});
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivros(req.params.id);
+    res.status(200).json(livros[index]);
 });
 
 //METODOS POSTS 
 app.post("/livros", (req, res) => {
-  livros.push(req.body);
-  res.status(201).send(`Livro adicionado com sucesso!`);
+    livros.push(req.body);
+    res.status(201).send(`Livro adicionado com sucesso!`);
 });
 
+//METODOS POST
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivros(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+});
 
-
+//METODOS DELETE
+app.delete("/livros/:id", (req, res)=>{
+    const index = buscaLivros(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro deletado com sucesso");
+});
 
 export default app;
